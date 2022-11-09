7683af82e2f1a753fc82b5c1846ee88237e4a4d0
VPagent <vpagent13@gmail.com>
2022-11-09 16:49:12 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = void 0;
+const App = () => {
+    return (<div style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101'
+        }}>
+      VPagent
+    </div>);
+};
+exports.App = App;
